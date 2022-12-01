7fae12b1204f21cb606f194007ae74276e0803ea
yusuf <af003yusuf@gmail.com>
2022-12-01 16:11:34 +0700

    .

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from yolo_deploy import customDetection
 
 
 def main():
-    detect = customDetection
+    detect = customDetection(capture_index=0,model_name='drone.pt')
     #tracker = CentroidTracker(maxDisappeared=200,maxDistance=120)
-    detect(capture_index=0,model_name='drone.pt')
-
+    detect()
 
 if __name__ == '__main__':
     main()

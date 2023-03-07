c480832fc1053049f71e5d9bb56fb65fbe80e55f
Yusuf <af003yusuf@gmail.com>
2023-03-07 14:35:35 +0700

    .

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from yolo_deploy import customDetection
 
 
 def main():
-    detect = customDetection(capture_index=0,model_name='drone_survivor.pt')
+    detect = customDetection(capture_index=0,model_name='yolov5n.pt')
     detect()
 
 if __name__ == '__main__':
